9a1b745c873cd28360d09180d96682734f4a1f8c
zafki3l <phamdinhthai2005@gmail.com>
2025-12-12 22:37:36 +0700

    update merge_sort

--- src/php_implementation/sort/merge_sort.php
+++ src/php_implementation/sort/merge_sort.php
@@ -1,7 +1,9 @@
 <?php
 
+$start = microtime(true);
+
 $array = [];
-$array = randomArrayGenerator($array, 100);
+$array = randomArrayGenerator($array, 100000);
 
 function randomArrayGenerator(array $array, int $number) {
     for ($i = 0; $i < $number; $i++) {
@@ -19,11 +21,58 @@ function mergeSort(array $array)
         return $array;
     }
 
-    $left_array = [];
-    $right_array = [];
+    $mid = (int) ($length / 2);
+
+    $left_array = slice($array, 0, $mid);
+    $right_array = slice($array, $mid, $length);
+
+    $left = mergeSort($left_array);
+    $right = mergeSort($right_array);
+
+    return merge($left, $right);
 }
 
-function slice(array $array, int $start, int $end)
+function slice(array $array, int $start, int $end): array
 {
+    $slicedArray = [];
 
+    for ($i = $start; $i < $end; $i++) {
+        $slicedArray[] = $array[$i];
+    }
+
+    return $slicedArray;
 }
+
+function merge(array $left, array $right): array
+{
+    $mergedArray = [];
+    $i = 0;
+    $j = 0;
+    
+    while ($i < count($left) && $j < count($right)) {
+        if ($left[$i] > $right[$j]) {
+            $mergedArray[] = $right[$j];
+            $j++;
+        } else {
+            $mergedArray[] = $left[$i];
+            $i++;
+        }
+    }
+
+    while ($i < count($left)) {
+        $mergedArray[] = $left[$i];
+        $i++;
+    }
+
+    while ($j < count($right)) {
+        $mergedArray[] = $right[$j];
+        $j++;
+    }
+
+    return $mergedArray;
+}
+
+print_r(mergeSort($array));
+
+$end = microtime(true);
+echo "Time: " . ($end - $start) . "s\n";
